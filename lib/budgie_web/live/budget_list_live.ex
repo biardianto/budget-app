518181123b5c829115a6875349aaee77cb1cb992
biardianto <biardianto@gmail.com>
2025-11-07 15:33:52 +0700

    budget

--- lib/budgie_web/live/budget_list_live.ex
+++ lib/budgie_web/live/budget_list_live.ex
@@ -35,7 +35,7 @@ defmodule BudgieWeb.BudgetListLive do
         class="bg-gray-100 text-gray-700 hover:bg-gray-200 hover:text-gray-800 px-3 py-2 rounded-lg flex items-center gap-2"
       >
         <.icon name="hero-plus" class="h-4 w-4" />
-        <span>New Budget</span>
+        <span>New Budget...</span>
       </.link>
     </div>
     <.table id="budgets" rows={@budgets}>
